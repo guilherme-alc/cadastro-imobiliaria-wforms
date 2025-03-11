d4af1c7ca6ebb29a7c9d3020af5ad0f568451d39
guilherme-alc <guilhermealc01@gmail.com>
2025-03-11 07:53:00 -0300

    feat: adiciona classe de conexao ao banco

---
+++ Database/Conexao.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace CadastroImobiliaria.Database
+{
+    public class Conexao
+    {
+        public static SqlConnection ObterConexao()
+        {
+            string connectionString = @"Server=GUICAM\SQLEXPRESS;Database=imobiliaria;User Id=sa;Password=123;TrustServerCertificate=True;";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Falha ao conectar com o banco de dados {ex.Message}");
+            }
+            return connection;
+        }
+        public static void FecharConexao(SqlConnection connection)
+        {
+            connection.Close();
+        }
+    }
+}
