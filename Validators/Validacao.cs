a65cc85928ac495d424e3dbd465aca3c6f76699e
guilherme-alc <guilhermealc01@gmail.com>
2025-03-11 07:54:35 -0300

    feat: Adiciona classe para validação dos campos de cadastro

---
+++ Validators/Validacao.cs
@@ -0,0 +1,125 @@
+﻿namespace CadastroImobiliaria.Validators
+{
+    public static class Validacao
+    {
+        public static void numeroInteiro(KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != (char)8)
+            {
+                e.Handled = true;
+            }
+        }
+        public static bool CPF(string cpf)
+        {
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            string tempCpf;
+            string digito;
+            int soma;
+            int resto;
+            cpf = cpf.Trim();
+            cpf = cpf.Replace(".", "").Replace("-", "");
+            if (cpf.Length != 11)
+                return false;
+            tempCpf = cpf.Substring(0, 9);
+            soma = 0;
+
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = resto.ToString();
+            tempCpf = tempCpf + digito;
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = digito + resto.ToString();
+            return true;
+        }
+
+        public static bool CNPJ(string cnpj)
+        {
+            int[] multiplicador1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int soma;
+            int resto;
+            string digito;
+            string tempCnpj;
+            cnpj = cnpj.Trim();
+            cnpj = cnpj.Replace(".", "").Replace("-", "").Replace("/", "");
+            if (cnpj.Length != 14)
+                return false;
+            tempCnpj = cnpj.Substring(0, 12);
+            soma = 0;
+            for (int i = 0; i < 12; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador1[i];
+            resto = (soma % 11);
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = resto.ToString();
+            tempCnpj = tempCnpj + digito;
+            soma = 0;
+            for (int i = 0; i < 13; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador2[i];
+            resto = (soma % 11);
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = digito + resto.ToString();
+            return cnpj.EndsWith(digito);
+        }
+
+        public static string CampoTexto(TextBox txt)
+        {
+            string erro;
+
+            if (txt != null && string.IsNullOrWhiteSpace(txt.Text))
+                erro = $"Preenchimento do campo obrigatório!";
+            else
+                erro = "";
+
+            return erro;
+        }
+        public static string CampoTextoMascara(MaskedTextBox txt)
+        {
+            string erro;
+
+            if (txt != null && string.IsNullOrWhiteSpace(txt.Text))
+                erro = $"Preenchimento do campo obrigatório!";
+            else if (!txt.MaskFull)
+                erro = "Preenchimento incompleto!";
+            else
+                erro = "";
+
+            return erro;
+        }
+
+        public static bool PreenchimentoCampo(TextBoxBase campo, string nomeCampo, ErrorProvider erro)
+        {
+            bool camposValidos = true;
+
+            if (string.IsNullOrEmpty(campo.Text))
+            {
+                erro.SetError(campo, $"O campo {nomeCampo} é obrigatório.");
+                camposValidos = false;
+            }
+            else
+            {
+                erro.SetError(campo, "");
+            }
+
+            return camposValidos;
+        }
+    }
+}
